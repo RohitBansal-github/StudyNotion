62bdbfb09342cb2ae3b332aabfa745172eb838fa
Rohit Bansal <rohitbansal7364@gmail.com>
2026-02-12 19:18:34 +0530

    fix cors issue

--- server/index.js
+++ server/index.js
@@ -22,11 +22,12 @@ dbconnect();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-    cors({
-        origin:"process.env.FRONTEND_URL",
-        credentials:true,
-    })
-)
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
 app.use(
     fileUpload({
         useTempFiles:true,
